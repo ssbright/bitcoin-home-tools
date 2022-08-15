f7ab8a42355a2f05f6999f00af5d853c59f5a1f5
Sydney <sydney@Sydneys-MacBook-Pro.local>
2022-08-15 09:42:30 -0700

    added mining info

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,13 @@ use bitcoincore_rpc::{Auth, Client, RpcApi};
 
 
 fn main() {
-
+    let addr = dotenv!("CARD");
     let rpc = Client::new(&dotenv!("IP").to_string(),
                           Auth::UserPass(dotenv!("RPC_USER").to_string(),
                                          dotenv!("RPC_PASSWORD").to_string())).unwrap();
     let best_block_hash = rpc.get_best_block_hash().unwrap();
+    let get_mining_info = rpc.get_mining_info().unwrap();
     println!("best block hash: {}", best_block_hash);
+    println!("get mining info: {:?}", get_mining_info);
 }
 
